f7396939abd9dee80af05f705c591a7e819b9402
Mounik <mounicou@gmail.com>
2018-06-27 12:51:20 +0200

    Modif functions.php de fonctions inutile

--- functions.php
+++ functions.php
@@ -26,30 +26,4 @@ function tsct_siteInfo() {
          '(Fièrement propulsé par <a href="' . esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ) . '" target="_blank" rel="noopener">WordPress</a>)';
 }
 
-/* Ajouter des sections à la page d'accueil */
 
-//function tsct_front_page_sections() {
-//    return 5; // la valeur souhaitée
-//}
-//add_filter( 'twentyseventeen_front_page_sections', 'tsct_front_page_sections' );
-//
-//function tsct_body_classes_child( $classes ){
-//if ( is_active_sidebar( 'sidebar-1' ) &&  is_page() ) {
-//        $classes[] = 'has-sidebar';
-//    }
-//    return $classes;
-//}
-//add_filter( 'body_class', 'tsct_body_classes_child' );
-
-/* Champ de recherche dans la barre de menu */
-//function add_search_box($items, $args) {
-//
-//        ob_start();
-//        get_search_form();
-//        $searchform = ob_get_contents();
-//        ob_end_clean();
-//
-//        $items .= '<li>' . $searchform . '</li>';
-//        return $items;
-//}
-//add_filter('wp_nav_menu_items','add_search_box', 10, 2);
